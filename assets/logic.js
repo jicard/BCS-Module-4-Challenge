6aed858f80f68a7a72f4680fac7e7490778a8c4d
jicard <icardj@outlook.com>
2022-10-08 15:36:12 -0400

    combined questions and logic

--- assets/logic.js
+++ assets/logic.js
@@ -1,7 +1,36 @@
+//Define "questions" variable, give each question a title string, choices array, and answer string
+var questions = [
+    {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts',
+    },
+    {
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses',
+    },
+    {
+      title: 'Arrays in JavaScript can be used to store ____.',
+      choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+      answer: 'all of the above',
+    },
+    {
+      title: 'String values must be enclosed within ____ when being assigned to variables.',
+      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+      answer: 'quotes',
+    },
+    {
+      title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+      answer: 'console.log',
+    },
+  ];
+  
 // variables to keep track of quiz state
 var currentQuestionIndex = 0;
-//var time = questions.length * 15;
-//var timerId;
+var time = questions.length * 15;
+var timerId;
 
 // variables to reference DOM elements
 var questionsEl = document.getElementById("questions");
@@ -19,6 +48,7 @@ function startQuiz() {
 viewHigh.style.display = "none";    
 startScreen.style.display = "none";
 questionsEl.style.display = "block";
+
 var sec = 60;
 var timer = setInterval(function(){
     time = '00:' + sec;
