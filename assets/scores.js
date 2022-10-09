ce4866a8845c8fba454c2cf7672d48c0bf130366
jicard <icardj@outlook.com>
2022-10-09 10:22:57 -0400

    scores page read/clear

--- assets/scores.js
+++ assets/scores.js
@@ -1,18 +1,18 @@
-#scores.js
-
 function printHighscores() {
   // either get scores from localstorage or set to empty array
-  var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+  var highscores = JSON.parse(window.localStorage.getItem('savedHighscores')) || [];
 
+/*
   // sort highscores by score property in descending order
   highscores.sort(function (a, b) {
     return b.score - a.score;
   });
+*/
 
-  for (var i = 0; i < highscores.length; i += 1) {
+  for (var i = 0; i < highscores.length; i ++) {
     // create li tag for each high score
     var liTag = document.createElement('li');
-    liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
+    liTag.textContent = highscores[i].name + ' - ' + highscores[i].score + "/5";
 
     // display on page
     var olEl = document.getElementById('highscores');
@@ -21,11 +21,11 @@ function printHighscores() {
 }
 
 function clearHighscores() {
-  window.localStorage.removeItem('highscores');
+  window.localStorage.removeItem('savedHighscores');
   window.location.reload();
 }
 
 document.getElementById('clear').onclick = clearHighscores;
 
 // run function when page loads
-printHighscores();
+window.onload = printHighscores();
